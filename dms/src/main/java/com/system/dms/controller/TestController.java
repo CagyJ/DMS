c8c2998324ff150e8160336f9c849abda7132811
CagyJiao <kaijie.jiao@instructure.com>
2021-10-09 23:50:07 +0200

    delete meaningless import and add html element in the test file

--- dms/src/main/java/com/system/dms/controller/TestController.java
+++ dms/src/main/java/com/system/dms/controller/TestController.java
@@ -3,7 +3,6 @@ package com.system.dms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -25,10 +24,4 @@ public class TestController {
   public String Testmwh(){
     return "Hello, i am the test!";
   }
-
-  // TODO for MWH: create your first response!
-  // do the similar thing as above, inside @GetMapping, you should write your name down, like /mwh
-  // and return a block of html code or just string, whatever you like
-  // finally, run the application and test your response, aka. response! (if your write /mwh into @GetMapping, you should enter localhost:8080/mwh to get your page!)
-
 }
