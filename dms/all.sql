3b1e395bf677ccfe1d8d2375e02d03a419dc1859
CagyJiao <kaijie.jiao@instructure.com>
2021-10-10 14:11:45 +0200

    set up database environment

--- dms/all.sql
+++ dms/all.sql
@@ -10,6 +10,8 @@ CREATE TABLE `order` (
   `status` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO dms.`order` (id, name, origin, destination, car_license, product_weight, status)
+    VALUES (1, 'test', 'budapest', 'german', 'a123', 123.6, 'completed');
 
 CREATE TABLE `car` (
   `id` bigint NOT NULL,
@@ -21,6 +23,8 @@ CREATE TABLE `car` (
   `driver_id_number` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO dms.car (id, car_license, driver_name, driver_phonenumber, status, driver_address, driver_id_number) VALUES (1, 'a123', 'Jack', 12345678, 'free', 'budapest', 'b123');
+
 
 CREATE TABLE `user` (
   `id` bigint NOT NULL,
@@ -28,3 +32,5 @@ CREATE TABLE `user` (
   `password` varchar(60) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO dms.user (id, username, password) VALUES (1, 'admin', 'admin');
+INSERT INTO dms.user (id, username, password) VALUES (2, 'test', 'test');
