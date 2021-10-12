e1ae8e7399ce5598c47d627e61432d993f25d2dd
CagyJiao <kaijie.jiao@instructure.com>
2021-10-12 13:42:18 +0200

    add index test and TestControllerTest test

--- dms/src/test/java/com/system/dms/DmsApplicationTests.java
+++ dms/src/test/java/com/system/dms/DmsApplicationTests.java
@@ -1,16 +1,33 @@
 package com.system.dms;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.system.dms.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DmsApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void indexTestReturn() throws Exception {
+		mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello World!")));
 	}
 
 }
