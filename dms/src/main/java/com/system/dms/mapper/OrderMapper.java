d2c12b59b54da0a8a79fe5a27a71463255d116f6
mwh <277424153@qq.com>
2021-10-12 11:05:18 +0200

    add Order Mapper

---
+++ dms/src/main/java/com/system/dms/mapper/OrderMapper.java
@@ -0,0 +1,11 @@
+package com.system.dms.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.system.dms.entity.Order;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderMapper extends BaseMapper<Order> {
+
+}
