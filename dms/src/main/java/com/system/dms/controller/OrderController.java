26e234b6094b60a83e897b0c35efc6ce68f1ce7c
mwh <277424153@qq.com>
2021-10-27 21:01:41 +0200

    make add and delete methods

--- dms/src/main/java/com/system/dms/controller/OrderController.java
+++ dms/src/main/java/com/system/dms/controller/OrderController.java
@@ -1,13 +1,14 @@
 package com.system.dms.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.system.dms.entity.Car;
 import com.system.dms.entity.Order;
+import com.system.dms.exception.DbRequestException;
 import com.system.dms.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +28,51 @@ public class OrderController {
         HashMap map=new HashMap();
         map.put("orders",orders);
         return map;
+    }
+
+    @PostMapping("/list")
+    @ResponseBody
+    public IPage<Order> list(@RequestParam("page") Integer page, @RequestParam("rows") Integer rows) {
+        if (page == null) {
+            page = 1;
+        }
+        if (rows == null) {
+            rows = 5;
+        }
+        IPage<Order> order = orderService.paging(page, rows);
+        return order;
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public Map addCar(Order order, Integer curPage, Integer pageSize, RedirectAttributes attr) {
+        Map result = new HashMap();
+        try {
+            orderService.addOrder(order);
+            result.put("code", "1");
+            result.put("message", "success");
+        } catch (DbRequestException e) {
+            result.put("code", e.getCode());
+            result.put("message", e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @PostMapping("/delete")
+    @ResponseBody
+    public Map deleteCar(Integer id) {
+        Map result = new HashMap();
+        try {
+            orderService.deleteOrder(id);
+            result.put("code", "1");
+            result.put("message", "success");
+        } catch (DbRequestException e) {
+            result.put("code", e.getCode());
+            result.put("message", e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
     }
 
 }
