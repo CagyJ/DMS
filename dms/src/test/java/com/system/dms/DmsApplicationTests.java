3f3adbd05d34e7eae3f65a905c5ede590f6785ef
CagyJiao <kaijie.jiao@instructure.com>
2021-10-29 23:27:14 +0200

    add page forward test and TODO

--- dms/src/test/java/com/system/dms/DmsApplicationTests.java
+++ dms/src/test/java/com/system/dms/DmsApplicationTests.java
@@ -2,12 +2,18 @@ package com.system.dms;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.system.dms.controller.PageController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -22,7 +28,28 @@ class DmsApplicationTests {
 		mockMvc.perform(get("/"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(view().name("index.html"));
+				.andExpect(view().name("index.html"))
+				.andExpect(handler().handlerType(PageController.class));
 	}
+
+	@Test
+	void ordersForwardedTest() throws Exception {
+		mockMvc.perform(get("/orders"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("orders.html"))
+				.andExpect(handler().handlerType(PageController.class));
+	}
+
+	@Test
+	void carsForwardedTest() throws Exception {
+		mockMvc.perform(get("/cars"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("cars.html"))
+				.andExpect(handler().handlerType(PageController.class));
+	}
+
+	// TODO test login, histories and greeting page
 
 }
